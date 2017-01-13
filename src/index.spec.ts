7ea9c911251ec34650daf3ca3fc626550e18dfd2
Hendrik Liebau <mail@hendrik-liebau.de>
2017-01-13 15:53:33 +0100

    fix: align pathname, host and port in IE with other environments (#2)
    - A pathname should always start with a slash.
    - The implicit ports 80 (http) and 443 (https) are omitted by all other
      environments besides Internet Explorer.
    - Always set an empty string as fallback.
    
    BREAKING CHANGE: Setting an empty string as fallback value deviates
    from the Node.js implementation of `url.parse`.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,7 +2,7 @@ import { assert } from 'chai';
 import { parseURL } from './';
 
 describe('parseURL', () => {
-  it('returns an URL object for the given URL', () => {
+  it('returns an URL object for http://www.example.com:1234/foo?bar=1#baz', () => {
     const urlString = 'http://www.example.com:1234/foo?bar=1#baz';
     const expectedResult = {
       hash: '#baz',
@@ -13,6 +13,51 @@ describe('parseURL', () => {
       port: '1234',
       protocol: 'http:',
       search: '?bar=1'
+    };
+    assert.deepEqual(parseURL(urlString), expectedResult);
+  });
+
+  it('returns an URL object for https://www.example.com', () => {
+    const urlString = 'https://www.example.com';
+    const expectedResult = {
+      hash: '',
+      host: 'www.example.com',
+      hostname: 'www.example.com',
+      href: 'https://www.example.com/',
+      pathname: '/',
+      port: '',
+      protocol: 'https:',
+      search: ''
+    };
+    assert.deepEqual(parseURL(urlString), expectedResult);
+  });
+
+  it('returns an URL object for http://www.example.com', () => {
+    const urlString = 'http://www.example.com';
+    const expectedResult = {
+      hash: '',
+      host: 'www.example.com',
+      hostname: 'www.example.com',
+      href: 'http://www.example.com/',
+      pathname: '/',
+      port: '',
+      protocol: 'http:',
+      search: ''
+    };
+    assert.deepEqual(parseURL(urlString), expectedResult);
+  });
+
+  it('returns an URL object for http://www.example.com:1234?bar=1&baz=2', () => {
+    const urlString = 'http://www.example.com:1234?bar=1&baz=2';
+    const expectedResult = {
+      hash: '',
+      host: 'www.example.com:1234',
+      hostname: 'www.example.com',
+      href: 'http://www.example.com:1234/?bar=1&baz=2',
+      pathname: '/',
+      port: '1234',
+      protocol: 'http:',
+      search: '?bar=1&baz=2'
     };
     assert.deepEqual(parseURL(urlString), expectedResult);
   });
